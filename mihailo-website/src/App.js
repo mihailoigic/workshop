1040619bcc5195f8a4f707c9a89ea89f1590685e
latinovicmilica <milicalatinovic47@gmail.com>
2021-05-31 17:56:16 +0200

    initial commit

---
+++ mihailo-website/src/App.js
@@ -0,0 +1,13 @@
+import logo from './assets/img/cslogo.png';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
